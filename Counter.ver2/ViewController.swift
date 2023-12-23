6e36c12afd31e263e6164944e92fb071ee0fda14
TimurMiskurov <timurmiskurov@gmail.com>
2023-12-23 18:57:19 +0300

    Complete Task

--- Counter.ver2/ViewController.swift
+++ Counter.ver2/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var count = 0
+    
+    @IBOutlet weak var nullButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var countTextField: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        countTextField.text = "История изменений:"
+        plusButton.backgroundColor = .red
+        plusButton.tintColor = .white
+        minusButton.backgroundColor = .blue
+        minusButton.tintColor = .white
+        nullButton.tintColor = .black
+        nullButton.backgroundColor = .systemGray
+        
+        var formatter = DateFormatter()
+        formatter.dateFormat = "dd/MM/yyyy HH:mm:ss"
+        
     }
-
-
+    @IBAction func plusCount() {
+        count += 1
+        countTextField.text = "История изменений: \(count)"
+    }
+    
+    @IBAction func minusCount() {
+        count -= 1
+        countTextField.text = "История изменений: \(count)"
+    }
+    
+    
+    @IBAction func makeNull() {
+        count = 0
+        countTextField.text = "Значение сброшено на 0"
+    }
 }
 
